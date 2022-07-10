3238965386cd9266789c311bf1a9bdc940c65a76
Kantemir618 <94971031+KantemirDK@users.noreply.github.com>
2022-07-10 20:57:35 +0300

    Добавлено решение задачи № 8 из ДЗ первого семинара

---
+++ Zadacha#8(sem1)C#/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 8 из ДЗ по первому семинару
+// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+    Console.WriteLine("Введите число: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    int index = 0;
+    int count = 0;
+    int find = (N / 2);
+
+Console.WriteLine("Все четные числа от 1 до введенного числа: ");
+
+    while (index < find)
+    {
+        count = count + 2;
+        Console.Write(count);
+        Console.Write(" | ");
+        index = index + 1;
+    }
+    
