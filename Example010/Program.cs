557ef47af05b291b87177e57933a431cccc61e4e
Kantemir618 <94971031+KantemirDK@users.noreply.github.com>
2022-07-10 18:17:40 +0300

    Добавлен десятый проект

---
+++ Example010/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 16, 22, 36, 1143, 58, 346, 77, 148, 739, 1143 };
+
+int n = array.Length;
+int find = 1143;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine (index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
