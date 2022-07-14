ad511c54b2a767014b6eb81d2499782abe0a8e03
Kantemir618 <94971031+KantemirDK@users.noreply.github.com>
2022-07-14 23:38:27 +0300

    Решение десятой задачи второго семинара + дополнительная функция проверки четности найденной второй цифры

---
+++ Zadacha#10(sem2)C#/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное \
+// число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число от 100 до 999: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int result = duo(N);
+
+int duo(int N)
+{
+    int uno = N / 100;
+    int tres = N % 10;
+    return (N / 10) % 10;
+
+}
+int total = duo(N);
+Console.WriteLine("Второй цифрой введенного числа является: ");
+Console.WriteLine(total);
+
+
+Console.WriteLine("Проверить чётность найденной второй цифры? (Ведите Да или Нет)");
+string answer = Console.ReadLine();
+
+if (answer.ToLower() == "да")
+{
+if (total % 2 == 1)
+{
+    Console.WriteLine("Число нечетное и делится на два с остатком");
+}
+else
+{
+    Console.WriteLine("Число четное и делится на два без остатка");
+}
+}
+else
+{
+    Console.WriteLine("Спасибо, что применили ИИ кулькулятор! Успехов Вам!");
+}
