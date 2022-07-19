0d9b7e86c83316e4428a0bd2100ccd287ddb9dd1
Kantemir618 <94971031+KantemirDK@users.noreply.github.com>
2022-07-19 03:20:52 +0300

    Добавлено решение задачи 19 третьего семинара

---
+++ Zadacha#19(sem3)C#/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите пятизначное число для проверки: ");
+uint a = uint.Parse(Console.ReadLine());
+uint b = a;
+var col = new List<uint>();
+while (b > 0)
+{
+    col.Add(b % 10);
+    b = b / 10;
+}
+b = 0;
+col.Reverse();
+for (int mcol = 0; mcol < col.Count; mcol++)
+    b = b + col[mcol] * (uint)Math.Pow(10, mcol);
+if (a == b)
+    Console.WriteLine("Введённое значение является палиндромом");
+else
+    Console.WriteLine("Введённое значение не являтется палиндромом");
