a8355883d5ff7a686341e33808d8878a0a7e65a6
Kantemir618 <94971031+KantemirDK@users.noreply.github.com>
2022-08-08 02:08:21 +0300

    Решение задачи 36 семнара 5 (требуется доработка)

---
+++ Zadacha#36(sem5)C#/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Write($"Введи количество элементов массива: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+
+int RandomNumbers(int numbers, int min, int max)
+{
+    int[] random = new int[numbers];
+    int sum = 0;
+    Console.Write("Получившийся массив: ");
+
+    for (int i = 0; i < random.Length; i++)
+    {
+        random[i] = new Random().Next(min, max);
+
+        Console.Write(random[i] + " ");
+
+        if (i % 2 != 1)
+        {
+            sum = sum + random[i];
+        }
+    }
+    return sum;
+}
+
+int random = RandomNumbers(numbers, 1, 100);
+
+Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {random}");
